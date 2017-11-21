13b6e6c3982ba494d5ef145082a2f7bddbc5139d
19JIvan <19jivan@mlmc.vic.edu.au>
2017-11-21 12:38:05 +1100

    print munipulations

---
+++ sdf.py
@@ -0,0 +1,9 @@
+print('Hello World')
+
+name = input('You? ')
+age = int(input('Age ? '))
+
+print('Hello', name , age)
+print('Hello ' + name + ' ' + str(age))
+
+print('Hello {0} your are {1} years old'.format(name, age))
