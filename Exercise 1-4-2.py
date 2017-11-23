d6ec9f6b34f1fc33723e2f9f32dbf2de6b3d616e
19JIvan <19jivan@mlmc.vic.edu.au>
2017-11-23 14:32:18 +1100

    finished the exercise

--- Exercise 1-4-2.py
+++ Exercise 1-4-2.py
@@ -1,8 +1,8 @@
 #this is the str of the DNA pattern
 strmyDna = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
 
-#this splits the code for the first and secound coding region
-strmyRna0 = strmyDna[:63] + ' this is the first coding region and ' + strmyDna[90:] + ' is the secound coding region'
+#this splits the intron
+strmyRna = strmyDna[63:90]
 
 #this prints the finished product
-print(strmyRna0)
+print(strmyDna[:63] + strmyRna.lower() + strmyDna[90:])
