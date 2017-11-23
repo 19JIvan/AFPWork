38b3f10138d93276f20d945b6e4a5a336fb43b3d
19JIvan <19jivan@mlmc.vic.edu.au>
2017-11-23 11:29:24 +1100

    DNA replace dna

---
+++ Exercise 1-2.py
@@ -0,0 +1,29 @@
+#The DNA sequance
+strmyDna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+
+#Replacing A with c
+strmyRna0 = strmyDna.replace("A", "c")
+
+#Printing to show the steps
+print(strmyRna0)
+
+#Replacing C with a
+strmyRna1 = strmyRna0.replace("C", "a")
+
+#Printing to show the steps
+print(strmyRna1)
+
+#Replacing T with g
+strmyRna2 = strmyRna1.replace("T", "g")
+
+#Printing to show the steps
+print(strmyRna2)
+
+#Replacing G with t
+strmyRna3 = strmyRna2.replace("G", "t")
+
+#Printing to show the steps
+print(strmyRna3)
+
+#Printing and changing to upper case
+print(strmyRna3.upper())
